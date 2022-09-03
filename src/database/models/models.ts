5b1edf52792867777e2891a3f87164ce6f6d4136
MarkSmersh <marksmersh01@gmail.com>
2022-09-03 04:41:37 +0300

    Added types to models

--- src/database/models/models.ts
+++ src/database/models/models.ts
@@ -1,22 +1,43 @@
 import sequelize from "../database";
-import { DataTypes as DT } from "sequelize";
+import { DataTypes as DT, CreationOptional, InferAttributes, InferCreationAttributes, Model, ModelDefined, Optional } from 'sequelize';
 
-export const UserState = sequelize.define("user-state", {
+export class UserState extends Model<InferAttributes<UserState>, InferCreationAttributes<UserState>> {
+    declare user_id: number;
+    declare state: string;
+}
+
+UserState.init({
     user_id: { type: DT.INTEGER, unique: true },
     state: { type: DT.STRING }
-})
+}, { tableName: 'user_states', sequelize });
 
-export const Order = sequelize.define("order", {
+
+export class Order extends Model<InferAttributes<Order>, InferCreationAttributes<Order>> {
+    declare id: CreationOptional<number>;
+    declare order: string;
+    declare orderState: "created" | "packaged" | "sended";
+    declare phoneNumber: number;
+    declare fullName: string;
+    declare billingType: "prepaid" | "cash";
+    declare destination: string;
+    declare waybill: string;
+    declare createdBy: number;
+    declare createdAt: CreationOptional<Date>;
+    declare updatedBy: number;
+    declare updatedAt: CreationOptional<Date>;
+}
+
+Order.init({
     id: { type: DT.INTEGER, primaryKey: true, autoIncrement: true }, 
     order: { type: DT.STRING }, // example: 
-    order_state: { type: DT.STRING }, // "created" | "packaged" | "sended"
-    phone_number: { type: DT.NUMBER }, // +381234567890
-    full_name: { type: DT.STRING }, // `${SecondName} ${FirstName} ${Surname}`
-    billing_type: { type: DT.STRING }, // "prepaid" | "cash"
+    orderState: { type: DT.STRING }, // "created" | "packaged" | "sended"
+    phoneNumber: { type: DT.INTEGER }, // +381234567890
+    fullName: { type: DT.STRING }, // ${SecondName} ${FirstName} ${Surname}
+    billingType: { type: DT.STRING }, // "prepaid" | "cash"
     destination: { type: DT.STRING }, // get from novaposhta api
     waybill: { type: DT.INTEGER }, // get from novaposhta api
-    created_by: { type: DT.INTEGER }, // chat_id
-    created_at: { type: DT.DATE }, // 
-    updated_by: { type: DT.INTEGER }, // chat_id
-    updated_at: { type: DT.DATE }
-})
+    createdBy: { type: DT.INTEGER }, // chat_id
+    createdAt: { type: DT.DATE }, // 
+    updatedBy: { type: DT.INTEGER }, // chat_id
+    updatedAt: { type: DT.DATE }
+}, { tableName: 'orders', sequelize })
